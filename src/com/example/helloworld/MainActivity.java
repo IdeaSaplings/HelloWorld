0d7e696085a8455f116371d95ea9a7deb3b46c5b
Navine MC <mcnavine@outlook.com>
2014-09-21 16:08:43 +0530

    Cleaning the HelloWorld

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.helloworld;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 // Removed unwanted headers
-// Added v7 library
+// Added v7 library only
 
 public class MainActivity extends ActionBarActivity {
 	
